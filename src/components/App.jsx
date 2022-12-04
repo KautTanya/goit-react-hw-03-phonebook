f85ad0b27f18210f78e329c73ae529bc26b3cf7d
KautTanya <kaut.tanya.s@gmail.com>
2022-12-05 00:02:56 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,6 +53,22 @@ deleteContact = contactId => {
     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
   }));
 }
+componentDidMount() {
+  const contacts = localStorage.getItem('contacts');
+  const parsedContacts = JSON.parse(contacts);
+
+  if(parsedContacts) {
+    this.setState({contacts: parsedContacts});
+  }
+}
+
+componentDidUpdate(prevProps, prevState){
+  if(this.state.contacts !== prevState.contacts){
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
+
+
   render() {
     const { filter, contacts } = this.state;
       

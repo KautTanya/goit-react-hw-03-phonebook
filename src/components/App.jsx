4707f6963b841ce6422c2e7ca47a5e5b0ceedaa5
KautTanya <kaut.tanya.s@gmail.com>
2022-12-07 06:41:47 +0200

    !!!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,7 @@ export class App extends Component{
     }
   }
   
-  componentDidUpdate(prevProps, prevState){
+  componentDidUpdate(_, prevState){
     if(this.state.contacts !== prevState.contacts){
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
